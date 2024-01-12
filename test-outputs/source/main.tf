b124a651d382f340bbd2530f4e5f33d4e63d80aa
Shriram Rajaraman <shriramrajaraman@shriramrajaraman-d0w9hh9r30.bbrouter>
2024-01-12 14:31:22 +0530

    Updated resources

--- test-outputs/source/main.tf
+++ test-outputs/source/main.tf
@@ -2,28 +2,45 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
+# data "aws_ami" "amazon_linux" {
+#   most_recent = true
+#   owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+#   }
+# }
+
+# resource "aws_instance" "web" {
+#   ami           = data.aws_ami.amazon_linux.id
+#   instance_type = var.instance_type
+
+#   user_data = <<-EOF
+#     #!/bin/bash
+#     sudo yum update -y
+#     sudo yum install httpd -y
+#     sudo systemctl enable httpd
+#     sudo systemctl start httpd
+#     echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
+#     EOF
+
+#   tags = var.tags
+# }
+
+resource "random_integer" "number1" {
+  max=100
+  min = 1
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = var.instance_type
+resource "random_string" "name" {
+  length = 5
+}
 
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo yum update -y
-    sudo yum install httpd -y
-    sudo systemctl enable httpd
-    sudo systemctl start httpd
-    echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
-    EOF
+output "name" {
+  value = random_string.name
+}
 
-  tags = var.tags
+output "number" {
+  value = random_integer.number1
 }
--- test-outputs/source/outputs.tf
+++ test-outputs/source/outputs.tf
@@ -1,10 +1,10 @@
-output "domain_name" {
-  description = "Public domain name of EC2 instance"
-  value       = aws_instance.web.public_dns
-  sensitive = false
-}
+# output "domain_name" {
+#   description = "Public domain name of EC2 instance"
+#   value       = aws_instance.web.public_dns
+#   sensitive = false
+# }
 
-output "ec2_name" {
-  value = aws_instance.web.id
-  sensitive = false
-}
+# output "ec2_name" {
+#   value = aws_instance.web.id
+#   sensitive = false
+# }
